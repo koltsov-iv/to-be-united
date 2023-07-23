f1af1ea74846671873c1b882ea162526ec726a50
Ilya Koltsov <koltsov.iv@gmail.com>
2023-07-23 20:56:26 +0100

    small fixes

--- src/assets/languages/en.ts
+++ src/assets/languages/en.ts
@@ -20,31 +20,36 @@ const en = {
     status_badge: "In progress",
     media_subtitle: "In the photo, I am buying a drone for Dnipro-1 for my birthday last year.",
     body_description: `<div>
-        <p>Just had a conversation with my friends; they are currently stationed at the Limansk front and actively engaged in combat operations. They are going through a tough time and are in need of assistance, counting on our support.</p>
-        <p>Telegram channel of their unit: <a href="https://t.me/d1air">https://t.me/d1air</a></p>
-        <p>Commander: <a href="https://instagram.com/podvorchanskiy">https://instagram.com/podvorchanskiy</a></p>
+        <p>Just had a conversation with my friends; they are currently stationed at the Limansk front
+        and actively engaged in combat operations. They are going through a tough time and are in need of assistance,
+        counting on our support.</p>
       </div>
 
       <div class="font-bold mt-4">Background:</div>
       <div class="mt-2">
-        On my previous birthday, I purchased a Mavic-3 drone, and my buddies were thrilled with it. The drone performed remarkably well, but unfortunately, it got shot down.
+        On my previous birthday, I purchased a Mavic-3 drone, and my buddies were thrilled with it.
+        The drone performed remarkably well, but unfortunately, it got shot down.
       </div>
 
       <div class="font-bold mt-4">Relevance:</div>
 
       <div class="italic mt-2">Why Dnipro-1?</div>
-      <div class="ml-2">- I have friends in several units on the Donetsk fronts, and after talking to all of them, it's evident that Dnipro-1 requires urgent support.</div>
+      <div class="ml-2">- I have friends in several units on the Donetsk fronts, and after talking to all of them,
+      it's evident that Dnipro-1 requires urgent support.</div>
 
       <div class="italic mt-2">Why a Drone?</div>
       <div class="ml-2">
-        - The most pressing request was for drones, and I happen to have expertise in their procurement and delivery. However, they also need Ecoflow power stations.
+        - The most pressing request was for drones, and I happen to have expertise in their procurement and delivery.
+        However, they also need Ecoflow power stations.
       </div>
 
       <div class="italic mt-2">Why am I doing this independently rather than through a foundation?</div>
       <ul class="ml-2">
         <li> - I want to conserve the resources of volunteers; processing each request consumes their valuable time.</li>
-        <li> - I want to save donor resources; purchasing items directly is more cost-effective. I buy them offline in Europe and utilize tax-free at the border, allowing the saved money to be used for the next donation.</li>
-        <li> - I want to establish effective communication with the participants involved in this project. (About the project)</li>
+        <li> - I want to save donor resources; purchasing items directly is more cost-effective. I buy them offline in
+         Europe and utilize tax-free at the border, allowing the saved money to be used for the next donation.</li>
+        <li> - I want to establish effective communication with the participants involved in this project.
+        (About the project)</li>
       </ul>
       <div class="flex justify-start space-x-2 mt-4">
       <span>Media of the unit:</span>
