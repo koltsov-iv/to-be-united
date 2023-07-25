62d12ce777a738a879f1ead0465830bf30d03b06
Ilya Koltsov <koltsov.iv@gmail.com>
2023-07-25 23:18:52 +0100

    fix show more for mobile chrome

--- src/app/pages/fundraising/fundraising.component.ts
+++ src/app/pages/fundraising/fundraising.component.ts
@@ -33,8 +33,8 @@ export class FundraisingComponent implements OnInit {
     })
   }
 
-  isNotSafari(): boolean {
-    return this.deviceService.browser !== 'Safari'
+  showReadMore(): boolean {
+    return this.deviceService.browser !== 'Safari' && !this.deviceService.isMobile()
   }
 
   readMoreBody(): void {
